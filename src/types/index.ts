e4d2ad83598855473cebec7fc07bd5b5329022e0
michaski <michaski86@gmail.com>
2025-10-03 22:59:23 +0200

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,56 @@
+export type NoteValue = 
+  | 'whole' 
+  | 'half' 
+  | 'quarter' 
+  | 'eighth' 
+  | 'sixteenth' 
+  | 'triplet-eighth' 
+  | 'triplet-sixteenth';
+
+export interface Beat {
+  id: number;
+  isAccent: boolean;
+}
+
+export interface TimingMetrics {
+  clicksPerBar: number;
+  clicksPerSecond: string;
+  intervalMs: string;
+  barDurationMs: string;
+}
+
+export interface NoteValueOption {
+  value: NoteValue;
+  label: string;
+}
+
+// Component Props
+export interface TempoControlProps {
+  bpm: number;
+  onBpmChange: (bpm: number) => void;
+}
+
+export interface BeatPatternProps {
+  beats: Beat[];
+  onAddBeat: () => void;
+  onRemoveBeat: () => void;
+  onToggleAccent: (id: number) => void;
+  currentBeat?: number;
+}
+
+export interface NoteValueSelectorProps {
+  noteValue: NoteValue;
+  onNoteValueChange: (value: NoteValue) => void;
+}
+
+export interface GapClickControlsProps {
+  barsOn: number;
+  barsOff: number;
+  onBarsOnChange: (value: number) => void;
+  onBarsOffChange: (value: number) => void;
+}
+
+export interface PlaybackControlsProps {
+  isPlaying: boolean;
+  onTogglePlay: () => void;
+}
